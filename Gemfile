b950407fad099ad7bfba07aa4d319ab287f1b878
Yann Klis <yann.klis@novelys.com>
2015-02-13 19:10:03 +0100

    output of converters

--- Gemfile
+++ Gemfile
@@ -4,3 +4,4 @@ ruby '2.2.0'
 
 gem 'markdown'
 gem 'pry'
+gem 'nokogiri'
--- converter.rb
+++ converter.rb
@@ -1,4 +1,5 @@
 require 'kramdown'
+require 'nokogiri'
 require 'pry'
 
 row_identifier = "--- row ---"
@@ -41,6 +42,10 @@ Dir.glob(files).each{|input_filename|
 
   text_transformed = row_begin_tag + ary_of_rows_transformed.join(row_end_tag + row_begin_tag) + row_end_tag
 
+  File.open("_includes/#{basename}_toc.html", "w+") do |f|
+    f.puts table_of_contents(text_transformed)
+  end
+
   File.open(output_filename, "w+") do |f|
     f.puts '---'
     f.puts 'layout: default'
@@ -49,3 +54,32 @@ Dir.glob(files).each{|input_filename|
     f.puts text_transformed
   end
 }
+
+def body_for(resource)
+  resource.render(layout: nil)
+end
+
+def doc_for(resource)
+  html = body_for(resource)
+  Nokogiri::HTML::DocumentFragment.parse(html)
+end
+
+def toc_link(heading)
+  heading_id = heading[:id] || "#"
+  content_tag(:a, heading.text, href: "#" + heading_id)
+end
+
+def toc_item(heading)
+  content_tag(:li, toc_link(heading))
+end
+
+def heading_nodes(resource)
+  doc_for(resource).css('h2')
+end
+
+def table_of_contents(resource)
+  list = heading_nodes(resource).map do |heading|
+    toc_item(heading)
+  end.join
+  content_tag(:ul, list), class: "list-unstyled")
+end
